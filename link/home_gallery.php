f674c4d104b2c68cfe0833df1c568606bf335264
suphakit01 <43027793+suphakit01@users.noreply.github.com>
2019-11-15 05:39:06 +0700

    ...

--- link/home_gallery.php
+++ link/home_gallery.php
@@ -61,22 +61,22 @@
             <div class="row">
                 <?php
                 echo "<div class='col-sm-3'>";
-                for ($i = 1; $i <= 20; $i++) {
+                for ($i = 1; $i <= 11; $i++) {
                     echo "<img id='myImg' class='img_gallery myImages' src='../scr/gallery/col1/" . $i . ".jpg'>";
                 }
                 echo "</div>";
                 echo "<div class='col-sm-3'>";
-                for ($i = 1; $i <= 20; $i++) {
+                for ($i = 1; $i <= 11; $i++) {
                     echo "<img id='myImg' class='img_gallery myImages' src='../scr/gallery/col2/" . $i . ".jpg'>";
                 }
                 echo "</div>";
                 echo "<div class='col-sm-3'>";
-                for ($i = 1; $i <= 20; $i++) {
+                for ($i = 1; $i <= 10; $i++) {
                     echo "<img id='myImg' class='img_gallery myImages' src='../scr/gallery/col3/" . $i . ".jpg'>";
                 }
                 echo "</div>";
                 echo "<div class='col-sm-3'>";
-                for ($i = 1; $i <= 20; $i++) {
+                for ($i = 1; $i <= 10; $i++) {
                     echo "<img id='myImg' class='img_gallery myImages' src='../scr/gallery/col4/" . $i . ".jpg'>";
                 }
                 echo "</div>";
